7401b7a9798ad7d7962e370630ad9cfa4681e4ec
BT_1N3_34 <BT_1N3_34@BT_1N3_34.bktech.local>
2017-11-17 13:04:38 -0500

    dasfasdf

--- Labs/src/Lab32.java
+++ Labs/src/Lab32.java
@@ -6,6 +6,14 @@ public class Lab32
 		String test1 = "ape";
 		String test2 = "apple";
 		System.out.println(test2.compareTo(test1));
+		String[] test3 = {"butter","food","pineapple","vaseline"};
+		String[] test4 = {"apple","lasagna","lobsters","Yoga"};
+		String[] testProduct = merge(test3,test4);
+		
+		for (String x: testProduct)
+		{
+			System.out.print("["+x+"] ");
+		}
 	}
 	public static String[] merge( String[] list1, String[] list2)
 	{
@@ -13,8 +21,10 @@ public class Lab32
 		int nextIndexOutput = 0;
 		for (String x : list1)
 		{
+			System.out.println(x);
 			for (String y: list2)
 			{
+				System.out.println(y);
 				if (x.compareTo(y)>0)
 				{
 					output[nextIndexOutput] = y;
