ec5fb2c06d45b65da3bf58490cb337c5d4a00a0a
BT_1N3_34 <BT_1N3_34@BT_1N3_34.bktech.local>
2017-12-15 13:14:15 -0500

    sdghh

--- Labs/src/lab4/Deck.java
+++ Labs/src/lab4/Deck.java
@@ -1,13 +1,32 @@
 package lab4;
-
+import java.util.List;
 import java.util.ArrayList;
 
 public class Deck {
-	ArrayList<Card> unDealt;
-	ArrayList<Card> Dealt;
+	private List<Card> unDealt = new ArrayList<Card>();
+	private List<Card> Dealt = new ArrayList<Card>();
 	public Deck(String[]rank, String[]suit, int[]pointValue)
 	{
+		for(int r=0;r< rank.length; r++)
+		{
+			for(int s=0;s< suit.length;s++)
+			{
+				for(int p=0;p<pointValue.length;p++)
+				{
+					unDealt.add(new Card(rank[r],suit[s],pointValue[p]));
+					
+				}
+			}
+		}
 		
+	}
+	public boolean isEmpty()
+	{
+		if(Deck.length>0)
+		{
+			return true;
+		}
+		return false;
 	}
 
 }
